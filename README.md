1f61b6ff87b9c09d525bca77e71711db4cc4500f
keegers <nikkikiga@gmail.com>
2020-03-26 13:24:35 -0700

    add through fizzBuzz

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# lab-04a-tested-functions
+# First Lab with Tests
+
+-----
+## Stuff Used
+- first time using ES lint
+- first time using Qunit for testing
+
+-----
+## Tips Learned
+- make sure to always set a negative test first to make sure you're not getting FALSE POSITIVES
+- 
+
+-----
+## If Continued, Would Follow Up On
+- functions that are passed functions (what does that mean for scope, parameters, execution etc.)
+- 
--- test/tests.js
+++ test/tests.js
@@ -21,6 +21,42 @@ function isEven (x) {
     return x % 2 === 0;
 }
 
+function getNumbers(upTo) {
+    const numbers = [];
+    for(let i = 1; i <= upTo; i++) {
+        numbers.push(i);
+    }
+    return numbers;
+}
+
+function getEvenNumbers(upTo) {
+    const numbers = [];
+    for(let i = 1; i <= upTo; i++) {
+        let checkEven = isEven(i);
+        if(checkEven) {
+            numbers.push(i);
+        }
+    }
+    return numbers;
+}
+
+function getFizzBuzzNumbers(upTo) {
+    const numbers = [];
+
+    for(let i = 1; i < upTo; i++) {
+        if(i % 3 === 0 && i % 5 === 0) {
+            numbers.push('fizzbuzz');
+        } else if(i % 3 === 0) {
+            numbers.push('fizz');
+        } else if(i % 5 === 0) {
+            numbers.push('buzz');
+        } else {
+            numbers.push(i);
+        }
+    }
+    return numbers;
+}
+
 test('Addition test 1', function(assert) {
     //Arrange
     // Set up your parameters and expectations
@@ -138,4 +174,52 @@ test('Check even number test 2', function(assert) {
     // Make assertions about what is expected valid result
     //Forma is always actual, expected
     assert.equal(result, expected);
+});
+
+test('Return numbers up to', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const upTo = 5;
+    const expected = [1, 2, 3, 4, 5];
+
+    //Act
+    const numbers = getNumbers(upTo);
+    // Call the function you're testing and set the result to a const
+
+    //Assert
+    // Make assertions about what is expected valid result
+    //Forma is always actual, expected
+    assert.deepEqual(numbers, expected);
+});
+
+test('Return even numbers up to', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const upTo = 9;
+    const expected = [2, 4, 6, 8];
+
+    //Act
+    const numbers = getEvenNumbers(upTo);
+    // Call the function you're testing and set the result to a const
+
+    //Assert
+    // Make assertions about what is expected valid result
+    //Forma is always actual, expected
+    assert.deepEqual(numbers, expected);
+});
+
+test('Fizz Bizz Test', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const upTo = 16;
+    const expected = [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizzbuzz'];
+
+    //Act
+    const numbers = getFizzBuzzNumbers(upTo);
+    // Call the function you're testing and set the result to a const
+
+    //Assert
+    // Make assertions about what is expected valid result
+    //Forma is always actual, expected
+    assert.deepEqual(numbers, expected);
 });
